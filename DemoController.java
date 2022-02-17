f3c04c64fb54a84a73783b635685d7a7cea7abca
inform2manikandan <99901812+inform2manikandan@users.noreply.github.com>
2022-02-17 23:45:30 +0530

    Add files via upload

---
+++ DemoController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+@RequestMapping("/")
+public class DemoController {
+
+	
+	
+	
+ 	    @RequestMapping(value="/demo", method=RequestMethod.GET)
+	    public String demo()
+	    {
+	        // The html file name is helloWorldPage.html.
+ 	    	System.out.println("Hi");
+	        return "helloWorldPage";
+	    }
+	
+}
