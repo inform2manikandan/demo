67b222ffde6fa24bbd7e48e12f186fa5f0bce15e
Mani <inform2manikandan@gmail.com>
2022-02-20 11:41:25 +0530

    TES-15 #comment updated the output

--- DemoController.java
+++ DemoController.java
@@ -11,7 +11,7 @@ public class DemoController {
 	    public String demo()
 	    {
 	        // The html file name is helloWorldPage.html.
- 	    	System.out.println("Hi updated 2nd time:...");
+ 	    	System.out.println("Hi updated 3rd time:...");
 	        return "helloWorldPage";
 	    }
 	
