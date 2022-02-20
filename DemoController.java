93cf8299a64e67b3a010b507faac3987b4252f77
Mani <inform2manikandan@gmail.com>
2022-02-20 11:50:59 +0530

    TES-15 #comment updated agian for testing2 #IN-REVIEW

--- DemoController.java
+++ DemoController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class DemoController {
 
-
-
  	    @RequestMapping(value="/demo", method=RequestMethod.GET)
 	    public String demo()
 	    {
